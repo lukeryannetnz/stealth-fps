15004a780138efab2455b5485ead06e122bd82ec
Luke Ryan <github@lukeryan.net.nz>
2019-09-10 20:30:56 +1200

    Formatting prettier

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -7,20 +7,18 @@
 #include "Engine/EngineTypes.h"
 
 // Sets default values
-AFPSExtractionZone::AFPSExtractionZone()
-{
-	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
-	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	OverlapComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
-	OverlapComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
-	OverlapComp->SetBoxExtent(FVector(200.0F));
-	RootComponent = OverlapComp;
+AFPSExtractionZone::AFPSExtractionZone() {
+    OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+    OverlapComp -> SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    OverlapComp -> SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    OverlapComp -> SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+    OverlapComp -> SetBoxExtent(FVector(200.0 F));
+    RootComponent = OverlapComp;
 
-	OverlapComp->SetHiddenInGame(false);
-	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+    OverlapComp -> SetHiddenInGame(false);
+    OverlapComp -> OnComponentBeginOverlap.AddDynamic(this, & AFPSExtractionZone::HandleOverlap);
 }
 
-void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
-{
-	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone"));
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
+    UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone"));
 }
