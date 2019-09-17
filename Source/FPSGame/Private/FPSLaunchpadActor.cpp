02eb84ab8d65ec4515aec4d66b0054837318b574
Luke Ryan <github@lukeryan.net.nz>
2019-09-18 08:01:59 +1200

    Added launchpad class

---
+++ Source/FPSGame/Private/FPSLaunchpadActor.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSLaunchpadActor.h"
+
+// Sets default values
+AFPSLaunchpadActor::AFPSLaunchpadActor()
+{
+}
+
+// Called when the game starts or when spawned
+void AFPSLaunchpadActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
